7c33d8303d47b8615077bb9ff5d00742105d00bc
EdiblePerspective <ryancox430@gmail.com>
2024-01-23 16:58:57 +0000

    attempt fix postid

--- src/app/Posts/[id]/page.js
+++ src/app/Posts/[id]/page.js
@@ -1,13 +1,13 @@
 
 import {sql} from "@vercel/postgres"
-export async function generateMetadata(){
-    const post = await sql(`SELECT * FROM posts WHERE id = $1`,[id]);
+export async function generateMetadata({params}){
+    const post = await sql`SELECT * FROM posts WHERE id = ${params.id}`;
     return {
         title: `${post.title} | RC-Portfolio`,
         description: `${post.content} | RC-Portfolio`}}
 
-export default async function SelectedPost() {
-    const post = await sql(`SELECT * FROM posts WHERE id = $1`,[id]);
+export default async function SelectedPost({params}) {
+    const post = await sql`SELECT * FROM posts WHERE id = ${params.id}`;
     return (
     <>
     {post.rows.map((mappedPost)=>{
--- src/app/Posts/page.js
+++ src/app/Posts/page.js
@@ -8,7 +8,7 @@ export default async function PostsLinks() {
   {posts.rows.map((post)=>{
     return(
       <div key={post.id}>
-        <Link href={`/posts${post.id}`}>{post.title}, {post.username}</Link>
+        <Link href={`/posts/${post.id}`}>{post.title}, {post.username}</Link>
       </div>)})};
   </>
   );}
